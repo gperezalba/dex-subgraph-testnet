64304f5472dc4c0a69ff4aab5a5813e571c71298
gperezalba <gperezalba94@gmail.com>
2021-02-16 16:05:11 +0100

    create pnft template

--- src/controller.ts
+++ src/controller.ts
@@ -4,6 +4,7 @@ import {
 } from "../generated/Controller/Controller"
 import { createPackable } from "./packable";
 import { createToken } from "./token";
+import { PNFTInterface as PNFTInterfaceTemplate } from "../generated/templates"
 
 export function handleNewToken(event: NewToken): void {
   createToken(event.params.newToken);
@@ -11,5 +12,6 @@ export function handleNewToken(event: NewToken): void {
 
 export function handleNewPNFToken(event: NewPNFToken): void {
   createToken(event.params.newToken);
+  PNFTInterfaceTemplate.create(event.params.newToken);
   createPackable(event);
 }
